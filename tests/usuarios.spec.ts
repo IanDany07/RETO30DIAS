c545fc427d94668ebd8cc34492d53a72a2526a82
IANDANY <iandany07@gmail.com>
2026-07-19 12:10:08 -0500

    Reto 2 - codigo actualizado

--- tests/usuarios.spec.ts
+++ tests/usuarios.spec.ts
@@ -10,11 +10,14 @@ test('usuarios', async({page})=>{
     await expect(page.getByRole('link',{name:'Admin'})).toBeVisible()
     //page.getByRole('link',{name:'Menu'}) = localizador
     await page.getByRole('link',{name:'Admin'}).click()
+    //getBytext=encontra elemento por texto (User managment) dentro del elemento (topbar Menu)
     await page.getByRole('navigation',{name:'Topbar Menu'}).getByText('User Management').click()
     await page.getByRole('menuitem',{name:'Users'}).click()
 
     //ahora toca identifiacar la tabla y luego capturamos toda la fila
-    // Capturamos toda la fila con "Const rows", ya no va el "await" por que no estoy haciendo una accion solo estamos capturando
+    // Capturamos toda la fila con "Const rows", ya no va el "await" 
+    // por que no estoy haciendo una accion solo estamos capturando
+    // capturamos toda la fila con rows desde una tabla, y capturamos la fila con el nombre (row) de la pagina
     const rows = page.getByRole('table').getByRole('row')
 
     // Crear un arreglo que va almacenar todo los nombres de usuarios (usernames).
@@ -22,27 +25,28 @@ test('usuarios', async({page})=>{
     const usernames:string[]=[]
     // de la tabla, saltar la primera fila. debemos Tener un contador para hacer una iteracion, para eso
     // debemos hacer el ciclo FOR.
-
-    // voy a contar cuantas filas hay, "await rows.count()" = si hay 8 usuarios mas la cabecera seria 9 
+    // voy a contar cuantas filas hay con "await rows.count()" = si hay 8 usuarios mas la cabecera seria 9 
     const rowcount = await rows.count()
      for(let i=1; i< rowcount; i++){
         // para acceder a la fila. // con nth accedo a un elemento que es (rows)
         // entonces, en la 1era iteracion accedo a la fila 2.
-        //Dentro de rows existe varias cell.
+        //Dentro de rows existe varias cell. (nth es la clave)-> cell es el nombre del rol de la subfila
+        //nth(1) es la segunda columna de la tabla.
         const cell=rows.nth(i).getByRole('cell').nth(1)
-        // el indice empieza en cero.
+        // el indice empieza en cero. El TextContent es un metodo para capturar el texto. Puede capturar un texto o puede ser nulo
+        // -> el texto capturado lo tenemos que agregarlo al arreglo (usernames)
         const username = await cell.textContent()
-        // si el texto no viene nullo, entonces agregalo, 
+        // El push(usermame) si o si espera un estring (texto) entonces
+        // hacemos la condicion if(){}
         if (username){
+            //si el texto no viene nullo, entonces agregalo a usernames desde username=texto capturado
             usernames.push(username)
         }
 
-    //imprime todo
-    console.log(usernames)
-
-
-
      }
-
+    //imprime todo los usuarios de la interface grafica
+    console.log(usernames)
+     
+    //EL RETO ES DE CREAR UN NUEVO SCRIP DONDE DEVOLVEMOS LOS NOMNBRES DE LOS EMPLEADOS
 
 })
